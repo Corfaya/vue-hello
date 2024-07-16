4e3ce33d250635e8b730e9d0fb34545cec057f9d
Corfaya <lauracorrado0696@gmail.com>
2024-07-16 15:27:53 +0200

    [UPD] BONUS DONE

--- bonus/js/app.js
+++ bonus/js/app.js
@@ -3,7 +3,11 @@ const { createApp } = Vue
 createApp({
     data(){
         return {
-            message: "Hello Vue"
+            message: "Hello Vue",
+            
+                img: "./img/procione.jpg",
+                name: "procione"
+          
         }
     }
 }).mount("#app")
